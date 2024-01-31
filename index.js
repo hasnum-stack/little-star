5cb8b48bc8ce4cc96fdd11386384ba79b4a0cab3
hasnum <hasnum@163.com>
2024-01-31 07:13:34 +0000

    feat:test npm publish beta pkg

---
+++ index.js
@@ -0,0 +1,12 @@
+function plusone (num){
+  return num + 1;
+}
+function plustwo(mum){
+  return num + 2;
+}
+function plusthree(num){
+  return num + 3;
+}
+export default plusone;
+export { plustwo, plusthree }; 
+
